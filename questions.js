b137b3bacac339075f5311eb7056bd641db55e3f
Astanzi-tech <70384069+Astanzi-tech@users.noreply.github.com>
2021-09-14 17:43:38 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,19 @@
+var questions1 = [
+{
+'question':'deficere', 'options': ['distruggere','disfare','mancare','mangiare'], 'correct':'mancare', 'suggestion':'si usa deficiente per indicare una persona con poca intelligenza'
+},
+{
+'question':'mittere', 'options': ['incontare','inviare','accogliere','impazzire'], 'correct':'inviare', 'suggestion': 'con mittente si indica colui che invia un messaggio'
+},
+{
+'question':'legere', 'options': ['leggere', 'eleggere', 'accogliere', 'pacificare'], 'correct':'eleggere', 'suggestion': 'non è l'/'azione che compi con qualcosa comprato in libreria'
+},
+{
+ 'question':'acutus', 'options': ['acuto', 'pungente','simpatico','sommo'], 'correct':'pungente', 'suggestion': 'acerbo indica un gusto pungente'
+},{
+    'question':'mālus', 'options': ['malevole', 'cattvo', 'malato', 'melo'], ' correct':'melo', 'suggestion': 'non è cio che pensi'
+},{
+    'question':'mălus', 'options': ['melo', 'penalita', 'cattivo', 'malato'], 'correct':'cattivo', 'suggestion':'non guardare la risposta precedente...'
+},
+];
+
---
+++ quiz.js
@@ -0,0 +1,78 @@
+var index = 0,
+    correct,
+    nextButton = document.getElementById('Next'),
+    StartButton = document.getElementById('Start'),
+    buttons = [],
+    GenerateQuiz = () => {
+    correct = 0;
+    nextButton.style = 'display:none';
+StartButton.style = 'display:none';
+generateQuestion(index);
+nextButton.onclick = () => {generateQuestion()}
+    }
+function generateQuestion(){
+    var container = document.querySelector('.container');
+    var question = questions1[index];
+    Title = document.createTextNode(question.question);
+    Title.className = 'ProperQuestion';
+    container.appendChild(Title);
+    for(option of question.options){
+        var Btn = document.createElement('button');
+            Btn.innerText = option;
+            Btn.value = option;
+            Btn.classList.add('button','options');
+            Btn.style.background = 'white'; 
+        container.appendChild(Btn);
+        buttons.push(Btn);
+        nextButton.style.display = 'none';
+        CheckCorrect(Btn,question);}
+    index++;
+    if (index == questions1.length){
+        endquiz(container);
+    }
+}
+function CheckCorrect(button, question){
+    question = question;
+    Btn = button;
+    console.log(question)
+    if (Btn.value === question.correct){
+        Btn.onclick = () =>{
+        buttons.forEach(Btn => Btn.disabled = true);
+        nextButton.style.display = 'inline';
+        button.style.background = 'green';
+        correct++
+        }
+       }
+       else{
+       Btn.onclick = () =>{
+        buttons.forEach(Btn => Btn.disabled = true);
+           nextButton.style.display = 'inline';
+           button.style.background = 'red';
+        }
+       }
+    }
+function endquiz(element){ 
+   var name = element;
+   nextButton.innerText = 'end quiz';
+   index = 0;
+   nextButton.onclick = () =>{
+       //nextButton.style = 'display:none';
+       while(name.firstChild){
+           name.removeChild(name.lastChild);
+       }
+       let previous = 0;
+       var score = document.createTextNode('hai finito il quiz con un punteggio di '+ (correct) +'/'+ questions1.length)
+            name.appendChild(score);
+            nextButton.innerText = 'ricomincia il quiz';
+            nextButton.onclick = () => {
+                name.removeChild(score)
+                nextButton.innerText = 'prossimo';
+                GenerateQuiz();
+            }
+            StartButton.style.display = 'inline-block';
+            StartButton.innerText = 'Ritorna alla pagina principale';
+            StartButton.onclick = () => {
+                window.open('landing-page.html');
+            }
+   }
+}
