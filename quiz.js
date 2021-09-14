268bf9530942193343577e1e90547565ae624c01
Astanzi-tech <70384069+Astanzi-tech@users.noreply.github.com>
2021-09-14 18:07:34 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,45 +2,27 @@ var index = 0,
     correct,
     nextButton = document.getElementById('Next'),
     StartButton = document.getElementById('Start'),
-    buttons = [],
-    GenerateQuiz = () => {
+    buttons = []
+    
+
+var GenerateQuiz = () => {
     correct = 0;
     nextButton.style = 'display:none';
-StartButton.style = 'display:none';
+    StartButton.style = 'display:none';  
 generateQuestion(index);
 nextButton.onclick = () => {generateQuestion()}
-    }
-function generateQuestion(){
-    var container = document.querySelector('.container');
-    var question = questions1[index];
-    Title = document.createTextNode(question.question);
-    Title.className = 'ProperQuestion';
-    container.appendChild(Title);
-    for(option of question.options){
-        var Btn = document.createElement('button');
-            Btn.innerText = option;
-            Btn.value = option;
-            Btn.classList.add('button','options');
-            Btn.style.background = 'white'; 
-        container.appendChild(Btn);
-        buttons.push(Btn);
-        nextButton.style.display = 'none';
-        CheckCorrect(Btn,question);}
-    index++;
-    if (index == questions1.length){
-        endquiz(container);
-    }
 }
+
 function CheckCorrect(button, question){
     question = question;
     Btn = button;
     console.log(question)
     if (Btn.value === question.correct){
         Btn.onclick = () =>{
-        buttons.forEach(Btn => Btn.disabled = true);
-        nextButton.style.display = 'inline';
-        button.style.background = 'green';
-        correct++
+            buttons.forEach(Btn => Btn.disabled = true);
+            nextButton.style.display = 'inline';
+            button.style.background = 'green';
+            correct++
         }
        }
        else{
@@ -51,6 +33,7 @@ function CheckCorrect(button, question){
         }
        }
     }
+
 function endquiz(element){ 
    var name = element;
    nextButton.innerText = 'end quiz';
